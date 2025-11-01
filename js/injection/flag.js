cedd1fe75c411e3c8545648f30dd4ed559ec5bee
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2025-11-01 14:06:20 +1100

    Refactor flag function to showFlag with alert

--- js/injection/flag.js
+++ js/injection/flag.js
@@ -1,9 +1,7 @@
-const flag = process.env.JS_INJECTION_FLAG;
-
-function flag() {
-       if (document.cookie.includes("complete=true")){
-              alert(flag);
-       } else {
-             alert("STOPP CHEATING IT DIDNT WORK");
-       }
+function showFlag() {
+  if (document.cookie.includes("complete=true")) {
+    alert("flag\{J5_1njEcti0n\}");
+  } else {
+    alert("STOPP CHEATING IT DIDNT WORK");
+  }
 }
